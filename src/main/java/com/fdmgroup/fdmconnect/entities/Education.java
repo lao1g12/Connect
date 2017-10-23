f837cd0e4504bd84f2edbe97fd9074d5d118a272
joseph.parker <joseph.parker@fdmgroup.com>
2017-10-23 15:35:09 +0100

    Entity classes complete.

--- src/main/java/com/fdmgroup/fdmconnect/entities/Education.java
+++ src/main/java/com/fdmgroup/fdmconnect/entities/Education.java
@@ -1,5 +1,112 @@
 package com.fdmgroup.fdmconnect.entities;
 
-public class Education {
+import java.util.Calendar;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="FC_EDUCATIONS")
+public class Education {
+	
+	@Id
+	@SequenceGenerator(name="educationid_sequence", sequenceName="educationid_sequence",
+	initialValue=1, allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="educationid_sequence")
+	@Column(name="ID")
+	private int educationId;
+	@Column(name="INSTITUTION")
+	private String institution;
+	@Column(name="QUALIFICATION")
+	private String qualification;
+	@Column(name="FURTHERINFO")
+	private String furtherInfo;
+	@Column(name = "STARTDATE")
+	private Calendar startDate;
+	@Column(name = "ENDDATE")
+	private Calendar endDate;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+	@JoinTable(name = "FC_EDUCATION_PROFILE")
+	private Profile profile;
+
+	
+	
+	public Education() {}
+
+	public Education(String institution, String qualification, String furtherInfo, Calendar startDate,
+			Calendar endDate) {
+		super();
+		this.institution = institution;
+		this.qualification = qualification;
+		this.furtherInfo = furtherInfo;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public int getEducationId() {
+		return educationId;
+	}
+
+	public void setEducationId(int educationId) {
+		this.educationId = educationId;
+	}
+
+	public String getInstitution() {
+		return institution;
+	}
+
+	public void setInstitution(String institution) {
+		this.institution = institution;
+	}
+
+	public String getQualification() {
+		return qualification;
+	}
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+
+	public String getFurtherInfo() {
+		return furtherInfo;
+	}
+
+	public void setFurtherInfo(String furtherInfo) {
+		this.furtherInfo = furtherInfo;
+	}
+
+	public Calendar getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Calendar startDate) {
+		this.startDate = startDate;
+	}
+
+	public Calendar getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Calendar endDate) {
+		this.endDate = endDate;
+	}
+
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+	
 }
--- src/main/java/com/fdmgroup/fdmconnect/entities/Profile.java
+++ src/main/java/com/fdmgroup/fdmconnect/entities/Profile.java
@@ -1,5 +1,156 @@
 package com.fdmgroup.fdmconnect.entities;
 
+import java.util.Calendar;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FC_PROFILES")
 public class Profile {
 
+	@Id
+	@SequenceGenerator(name = "profileid_sequence", sequenceName = "profileid_sequence", initialValue = 1, allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "profileid_sequence")
+	@Column(name = "ID")
+	private int profileId;
+	@Column(name = "FIRSTNAME")
+	private String firstName;
+	@Column(name = "LASTNAME")
+	private String lastName;
+	@Column(name = "STREAM")
+	private String stream;
+	@Column(name = "STARTDATE")
+	private Calendar startDate;
+	@Column(name = "ENDDATE")
+	private Calendar endDate;
+	@Column(name = "IMAGEURL")
+	private String imageUrl;
+	@Column(name = "DESCRIPTION")
+	private String description;
+	@Column(name = "HOBBIES")
+	private String hobbies;
+	@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.MERGE, mappedBy="profile")
+	private List<Education> education;
+	@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.MERGE, mappedBy="profile")
+	private List<Experience> experience;
+	
+	
+	
+	public Profile() {}
+
+	public Profile(String imageUrl, String description, String hobbies, List<Education> education,
+			List<Experience> experience) {
+		super();
+		this.imageUrl = imageUrl;
+		this.description = description;
+		this.hobbies = hobbies;
+		this.education = education;
+		this.experience = experience;
+	}
+
+	public int getProfileId() {
+		return profileId;
+	}
+
+	public void setProfileId(int profileId) {
+		this.profileId = profileId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getStream() {
+		return stream;
+	}
+
+	public void setStream(String stream) {
+		this.stream = stream;
+	}
+
+	public Calendar getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Calendar startDate) {
+		this.startDate = startDate;
+	}
+
+	public Calendar getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Calendar endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getHobbies() {
+		return hobbies;
+	}
+
+	public void setHobbies(String hobbies) {
+		this.hobbies = hobbies;
+	}
+
+	public List<Education> getEducation() {
+		return education;
+	}
+
+	public void addEducation(Education education) {
+		this.education.add(education);
+	}
+
+	public List<Experience> getExperience() {
+		return experience;
+	}
+
+	public void setExperience(List<Experience> experience) {
+		this.experience = experience;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
