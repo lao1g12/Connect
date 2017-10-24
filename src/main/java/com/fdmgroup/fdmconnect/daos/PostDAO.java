ea0125411693e4d22b7b071ad55bf293076b539a
liam.oliver <liam.oliver@fdmgroup.com>
2017-10-24 12:19:35 +0100

    addPost commit

--- src/main/java/com/fdmgroup/fdmconnect/daos/PostDAO.java
+++ src/main/java/com/fdmgroup/fdmconnect/daos/PostDAO.java
@@ -1,5 +1,9 @@
 package com.fdmgroup.fdmconnect.daos;
 
+import com.fdmgroup.fdmconnect.entities.Post;
+
 public interface PostDAO {
+	
+	public void addPost(Post post);
 
 }
--- src/main/java/com/fdmgroup/fdmconnect/daos/PostDAOImpl.java
+++ src/main/java/com/fdmgroup/fdmconnect/daos/PostDAOImpl.java
@@ -1,19 +1,34 @@
 package com.fdmgroup.fdmconnect.daos;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fdmgroup.fdmconnect.controllers.Logging;
+import com.fdmgroup.fdmconnect.entities.Post;
 
 public class PostDAOImpl implements PostDAO {
 
 	@Autowired
 	private EntityManagerFactory factory;
-	
-	public PostDAOImpl() {}
+
+	public PostDAOImpl() {
+	}
 
 	public PostDAOImpl(EntityManagerFactory factory) {
 		super();
-		this.factory = factory;
+		this.factory = factory;
+
 	}
-	
+
+	public void addPost(Post post) {
+		EntityManager manager = factory.createEntityManager();
+		manager.getTransaction().begin();
+		manager.persist(post);
+		manager.getTransaction().commit();
+		Logging.Log("info", "Post has been added" + post.getPostId());
+
+	}
+
 }
