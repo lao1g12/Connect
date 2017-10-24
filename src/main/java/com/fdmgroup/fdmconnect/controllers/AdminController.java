c80ebe1117fc0ecd005343690c01c4b92cde43d0
Gurneet.Singh <Gurneet.Singh@fdmgroup.com>
2017-10-24 12:37:58 +0100

    Admin Controller

---
+++ src/main/java/com/fdmgroup/fdmconnect/controllers/AdminController.java
@@ -0,0 +1,14 @@
+package com.fdmgroup.fdmconnect.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fdmgroup.fdmconnect.daos.PostDAOImpl;
+
+public class AdminController {
+	
+	
+	@Autowired
+	private PostDAOImpl postDao;
+	
+
+}
