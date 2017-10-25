42266a8a92eba5532334d4ed3d40200fce5afd91
olesya.lysykh <olesya.lysykh@TRNLON11500.fdmgroup.local>
2017-10-25 12:02:06 +0100

    FlagDAOImpl Merged.

--- src/main/java/com/fdmgroup/fdmconnect/daos/FlagDAOImpl.java
+++ src/main/java/com/fdmgroup/fdmconnect/daos/FlagDAOImpl.java
@@ -1,11 +1,33 @@
 package com.fdmgroup.fdmconnect.daos;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.fdmgroup.fdmconnect.entities.Flag;
 
 public class FlagDAOImpl implements FlagDAO {
 
+	@Autowired
+	private EntityManagerFactory factory;
+		
+	public FlagDAOImpl() {}
+
+	public FlagDAOImpl(EntityManagerFactory factory) {
+		super();
+		this.factory = factory;
+	}
+	
 	public void addFlag(Flag flag) {
 		
+		EntityManager manager = factory.createEntityManager();
+		manager.getTransaction().begin();
+		manager.persist(flag);
+		manager.getTransaction().commit();
+		
 	}
+	
+	
 
 }
