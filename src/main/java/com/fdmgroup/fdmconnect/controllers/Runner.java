0441cc3d24480f700f4fbe1a90621cfd8d03ff9b
joseph.parker <joseph.parker@fdmgroup.com>
2017-10-25 11:59:20 +0100

    FlagDAOImpl addFlag complete.

---
+++ src/main/java/com/fdmgroup/fdmconnect/controllers/Runner.java
@@ -0,0 +1,23 @@
+package com.fdmgroup.fdmconnect.controllers;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.fdmgroup.fdmconnect.daos.UserDAOImpl;
+import com.fdmgroup.fdmconnect.entities.Profile;
+import com.fdmgroup.fdmconnect.entities.User;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("DemoPersistence");
+		UserDAOImpl userDao = new UserDAOImpl(factory);
+		
+		Profile profile = new Profile("someUrl", "I am an admin!", "Stopping people having fun.");
+		User user = new User("admin","password","admin@fdmgroup.com","Admin","Admin");
+		user.setProfile(profile);
+		userDao.addUser(user);
+		
+	}
+
+}
