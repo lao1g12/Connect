d8f3a1a27dbefe790c89a50c788b05e8623d7d9f
Gurneet.Singh <Gurneet.Singh@fdmgroup.com>
2017-10-31 09:40:55 +0000

    Group Entity

---
+++ src/main/java/com/fdmgroup/fdmconnect/entities/Group.java
@@ -0,0 +1,119 @@
+package com.fdmgroup.fdmconnect.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FC_GROUPS")
+public class Group {
+	
+	
+	@Id
+	@SequenceGenerator(name = "groupid_sequence", sequenceName = "groupid_sequence", initialValue = 1, allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "groupid_sequence")
+	@Column(name = "ID")
+	private int groupId; 
+	private String name; 
+	private String description;
+	private String imageUrl; 
+	@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.MERGE, mappedBy="group")
+	private Set<Post> posts=new HashSet<Post>();
+	@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.MERGE, mappedBy="group")
+	private Set<User> users=new HashSet<User>();
+	
+		
+	
+	public Group() {}
+
+
+	public Group( String name, String description, String imageUrl) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.imageUrl = imageUrl;
+		
+	}
+
+
+	public int getGroupId() {
+		return groupId;
+	}
+
+
+	public void setGroupId(int groupId) {
+		this.groupId = groupId;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+
+	public Set<Post> getPosts() {
+		return posts;
+	}
+
+
+	public void setPosts(Set<Post> posts) {
+		this.posts = posts;
+	}
+	
+	
+	public void addPost(Post post){ 
+		this.posts.add(post);
+	}
+
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+	
+	public void addUser(User user){ 
+		this.users.add(user);
+	}
+	
+	
+
+}
