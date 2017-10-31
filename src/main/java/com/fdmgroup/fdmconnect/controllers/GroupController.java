3807a89d4b64d2d5e032035cd3e750c4f248a477
joseph.parker <joseph.parker@fdmgroup.com>
2017-10-31 10:33:40 +0000

    Fixing an almighty mess.

--- src/main/java/com/fdmgroup/fdmconnect/controllers/GroupController.java
+++ src/main/java/com/fdmgroup/fdmconnect/controllers/GroupController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fdmgroup.fdmconnect.daos.FlagDAOImpl;
+import com.fdmgroup.fdmconnect.daos.GroupDAOImpl;
 import com.fdmgroup.fdmconnect.daos.PostDAOImpl;
 import com.fdmgroup.fdmconnect.daos.UserDAOImpl;
 import com.fdmgroup.fdmconnect.entities.Flag;
+import com.fdmgroup.fdmconnect.entities.Group;
 
 @Controller
 public class GroupController {
@@ -36,9 +38,10 @@ public class GroupController {
 	@RequestMapping("/goToGroupHome")
 	public String admin(Model model, @RequestParam int groupId) {
 
-		Group group = groupDao.getGroup(1);
+		Group group = groupDao.getGroup(groupId);
 		model.addAttribute("allPosts", postDao.getAllPostsByGroup(groupId));
 		model.addAttribute("group", group);
 		return "user/GroupHome";
+		
 	}
 }
