e4daf4d4f4f6007b6dbf6e250c134567c6f14d7a
liam.oliver <liam.oliver@fdmgroup.com>
2017-11-01 18:08:01 +0000

    Try again

--- src/main/java/com/fdmgroup/fdmconnect/controllers/MessageController.java
+++ src/main/java/com/fdmgroup/fdmconnect/controllers/MessageController.java
@@ -78,10 +78,12 @@ public class MessageController {
 	
 	@RequestMapping("user/messages")
 	public String messageBox(@RequestParam String username, Principal principal, Model model){
+		MessageLogic ml = new MessageLogic();
 		User user = userDao.getUser(username);
 		User curUsername = userDao.getUser(principal.getName());
-		List<Notification> conversation = notifDao.getAllPostsByGroup(user, curUsername);
+		List<Notification> sent = notifDao.getAllPostsByGroup(user, curUsername);
 		System.out.println(user+" "+curUsername);
+		List<Notification> conversation = ml.refineMessages(sent, user, curUsername);
 //		List<Notification> conversation2 = notifDao.getAllPostsByGroup(curUsername, user);
 //		conversation.addAll(conversation2);
 		System.out.println(conversation);
