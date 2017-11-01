0c9e8c2be131ea4905df40b1e4a22821ebff596d
olesya.lysykh <olesya.lysykh@TRNLON11500.fdmgroup.local>
2017-11-01 14:13:46 +0000

    changes

--- src/main/java/com/fdmgroup/fdmconnect/controllers/GroupController.java
+++ src/main/java/com/fdmgroup/fdmconnect/controllers/GroupController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -89,12 +90,15 @@ public class GroupController {
 
 	
 	@RequestMapping("user/goToLeaveGroup")
-	public String goToLeaveGroup(Model model, HttpSession session, @RequestParam String username, @RequestParam String name, RedirectAttributes ra){
-		Group group =(Group)session.getAttribute("group");
-		List<User> users =userDao.getAllUsers();
-		//User owner = (User) session.getAttribute("user");
-	    // group.getOwner();
-	 groupDao .getGroup(name).getUsers().remove(username);
+	public String goToLeaveGroup(Model model, HttpSession session, @RequestParam(name="username" ) String username,  RedirectAttributes ra, Group groupId){
+        Group group =(Group)session.getAttribute("group");
+        List<User> users =userDao.getAllUsers();
+       
+        users.getGroup(groupId).getUsers().remove(username);
+     
+
+		System.out.println("1234");
+
 		ra.addFlashAttribute("ownerLeftGroup", "Owner left group  successfully");
 		return "redirect:/user/goToMyGroups";
 	}
@@ -121,7 +125,7 @@ public class GroupController {
 		model.addAttribute("group", group);
 		model.addAttribute("allPosts", postDao.getAllPostsByGroup(name));
 		
-		Notification notification = new Notification(title, type)
+		Notification notification = new Notification(title, type);
 		
 		
 	}
